3f0ce5fb257fc5e5350e0d5088fd5029690e9be1
Sergio HM <2308sergio@live.com.mx>
2025-08-17 17:03:35 -0600

    funcion agregar amigo

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 let amigos = [];
 
+function agregarAmigos(){
+    let ingresarAmigo = document.getElementById('amigo');
+    let nombres = ingresarAmigo.value.split(',').map(nombre => nombre.trim())
+
+    if(nombres.length === 0 || nombres[0] === ''){
+        alert('Ingresa un Nombre Valido');
+            return;
+        
+    }
+}
